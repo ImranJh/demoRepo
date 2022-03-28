b0061b8175cc8dcc5840e597fa01daeb388aa63b
Hp <Hp@DESKTOP-5E614SC>
2022-03-28 16:46:49 +0530

    Contact code added

---
+++ com.crm.vtiger.n/src/test/java/com/crm/Product/contactTest.java
@@ -0,0 +1,12 @@
+package com.crm.Product;
+
+import org.testng.annotations.Test;
+
+public class contactTest {
+	@Test
+	public void Contact()
+	{
+		System.out.println("Contact created successfully ");
+	}
+
+}
